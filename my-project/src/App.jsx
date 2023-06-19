b5e4d33a844dde3a0229a416d655bcc39a1af3a7
noklash <123834419+noklash@users.noreply.github.com>
2023-06-19 12:30:55 +0300

    initialized repo

---
+++ my-project/src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import logo from "../images/logo";
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+      {logo}
+    </h1>
+    </>
+  )
+}
+
+export default App
